03e187b6429ee8cf1b5758f646962ef8cdc42d72
akturtle <258859059@qq.com>
2015-11-17 14:34:36 +0800

    using kmeans results to initialize the EM of GMM and clear irrelevant code

--- p1b2.m
+++ p1b2.m
@@ -16,30 +16,29 @@ color=['r','g','b','m'];
 % title('EM for DataA')
 % saveas(h,['EM','_A'],'png');
 % 
-[mu, label,weight,cova]=myEm(dataB_X',K);
-points=dataB_X';
-h=figure;
-
-for i=1:K
-    classP=points(label==i,:);
-    scatter(classP(:,1),classP(:,2),color(i));
-    hold on;
-    plot(mu(i,1),mu(i,2),[color(i),'d'],'MarkerSize',10);
-    hold on;
-end
-title('EM for DataB')
-saveas(h,['EM','_B'],'png');
-% 
-% [mu, label]=myEm(dataC_X',K);
-% points=dataC_X';
+% [mu, label,weight,cova]=myEm(dataB_X',K);
+% points=dataB_X';
 % h=figure;
+% hold on;
 % for i=1:K
 %     classP=points(label==i,:);
 %     scatter(classP(:,1),classP(:,2),color(i));
-%     hold on;
 %     plot(mu(i,1),mu(i,2),[color(i),'d'],'MarkerSize',10);
-%     
-%     hold on;
+%     vl_plotframe([mu(i,:) cova(i,1) 0 cova(i,2)]);
 % end
-% title('EM for DataC')
-% saveas(h,['EM','_C'],'png');
+% title('EM for DataB')
+% saveas(h,['EM','_B'],'png');
+% 
+[mu, label,weight,cova]=myEm(dataC_X',K);
+points=dataC_X';
+h=figure;
+hold on;
+for i=1:K
+    classP=points(label==i,:);
+    scatter(classP(:,1),classP(:,2),color(i));
+    plot(mu(i,1),mu(i,2),[color(i),'d'],'MarkerSize',10);
+    vl_plotframe([mu(i,:) cova(i,1) 0 cova(i,2)]);
+end
+hold off;
+title('EM for DataC')
+saveas(h,['EM','_C'],'png');
